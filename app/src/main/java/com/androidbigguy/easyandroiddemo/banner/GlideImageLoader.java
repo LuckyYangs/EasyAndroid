2d0e1545b94fc7a608042b59c517bf2c0ab1694b
奔跑的蜗牛 <295803379@qq.com>
2018-09-07 09:38:56 +0800

    。

--- app/src/main/java/com/androidbigguy/easyandroiddemo/banner/GlideImageLoader.java
+++ app/src/main/java/com/androidbigguy/easyandroiddemo/banner/GlideImageLoader.java
@@ -3,16 +3,22 @@ package com.androidbigguy.easyandroiddemo.banner;
 import android.content.Context;
 import android.widget.ImageView;
 
-import com.bumptech.glide.Glide;
-import com.androidbigguy.easyandroiddemo.R;
 import com.androidbigguy.easyandroid.banner.loader.ImageLoader;
+import com.androidbigguy.easyandroiddemo.R;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 
 
 public class GlideImageLoader extends ImageLoader {
     @Override
     public void displayImage(Context context, Object path, ImageView imageView) {
-        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        Glide.with(context.getApplicationContext()).load(path).placeholder(R.drawable.nav_header_bg).into(imageView);
+//        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+//        Glide.with(context).load(path).placeholder(R.drawable.nav_header_bg).into(imageView);
+        RequestOptions options = new RequestOptions()
+                .centerCrop()
+                .diskCacheStrategy(DiskCacheStrategy.ALL);
+        Glide.with(context).load(path).apply(options).into(imageView);
     }
 
 }
